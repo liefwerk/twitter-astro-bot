da872e0518892b5738e37c47628549d1b57669fc
liefwerk <natjageni@gmail.com>
2021-08-27 20:10:06 +0000

    removed node cron job

--- index.js
+++ index.js
@@ -2,11 +2,5 @@ var cron = require('node-cron');
 var { addPlaylistItem } = require('./youtube-start')
 var { logger } = require('./log-index');
 
-
-cron.schedule('0 20 * * *', () => {
-  addPlaylistItem();
-  logger.info('cron job called');
-}, {
-  scheduled: true,
-  timezone: "Europe/Paris"
-});
+addPlaylistItem();
+logger.info('cron job called');
